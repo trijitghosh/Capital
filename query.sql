6b5e03a31a783a8e85c6e434fe838766d8f6ba3a
GhoshT <GhoshT@netrtl.com>
2022-05-27 22:41:57 +0100

    Full thing

--- query.sql
+++ query.sql
@@ -2,6 +2,6 @@
 --the new records by the first sponsor/collbarators.
 select count(1), leadsponsorname
 from clinic_trials
-where to_date(lastupdateposted, 'Month DD, yyyy')>current_date-4
+where to_date(lastupdateposted, 'Month DD, yyyy')>current_date-1
 group by leadsponsorname
 order by 1 desc;
