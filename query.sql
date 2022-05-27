4079a370d8d3196a7f036527929e19dd046d9548
GhoshT <GhoshT@netrtl.com>
2022-05-27 22:41:23 +0100

    Full thing

--- query.sql
+++ query.sql
@@ -1,3 +1,5 @@
+--This is the query for fetching count of
+--the new records by the first sponsor/collbarators.
 select count(1), leadsponsorname
 from clinic_trials
 where to_date(lastupdateposted, 'Month DD, yyyy')>current_date-4
